b8a2ccf1b575faf644667c67b968db837f4afb44
Carlo Liquido <carlo.h.liquido@gmail.com>
2016-03-27 16:15:02 -1000

    apply many-to-many relationship

--- flask/orm-tutorial/models-tutorial/app/models.py
+++ flask/orm-tutorial/models-tutorial/app/models.py
@@ -1,5 +1,8 @@
 from app import db
 
+orders=db.Table('orders',                             
+	db.Column('customer_id', db.Integer,db.ForeignKey('customer.id')),
+	db.Column('order_id',db.Integer,db.ForeignKey('order.id')))
 
 class Customer(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +13,9 @@ class Customer(db.Model):
 	phone = db.Column(db.String(10))
 	addresses = db.relationship('Address', backref='customer',
                                 lazy='dynamic')
+	orders=db.relationship('Order', secondary=orders, backref=db.backref('customer', lazy='dynamic') ) 
+	# orders=db.relationship('Order', secondary=orders, backref='customer',
+ #                                lazy='dynamic' ) 
     # You need to a relationship to Address table here
     # see http://flask-sqlalchemy.pocoo.org/2.1/models/#one-to-many-relationships
 
@@ -25,7 +31,7 @@ class Address(db.Model):
 	state = db.Column(db.String(120), unique=False)
 	country = db.Column(db.String(120), unique=False)
 	zip_code = db.Column(db.String(10), unique=False)
-	person_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
+	customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
 
 	def __repr__(self):
 		return '<Address %r>' % street_address
